93f28bc9d28a9927b3897ff7e43ea68e0dc9095e
Devansh Dubey <devanshdubey2001@gmail.com>
2021-01-17 14:41:06 +0530

    user authentication added

--- src/App.js
+++ src/App.js
@@ -1,48 +1,136 @@
 import React, { useState, useEffect } from 'react';
-import { db } from './firebase';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, Input } from '@material-ui/core';
+import { auth, db } from './firebase';
 import './App.css';
 import Post from './Post';
 
+function getModalStyle() {
+	const top = 50;
+	const left = 50;
+	return {
+		top: `${top}%`,
+		left: `${left}%`,
+		transform: `translate(-${top}%, -${left}%)`,
+	};
+}
+
+const useStyle = makeStyles(theme => ({
+	paper: {
+		position: 'absolute',
+		width: 400,
+		backgroundColor: theme.palette.background.paper,
+		border: '2px solid #000',
+		boxShadow: theme.shadows[5],
+		padding: theme.spacing(2, 4, 5),
+	},
+}));
+
 function App() {
-  const [posts, setPosts] = useState([
-    // {
-    //   username: "_cpp.freak",
-    //   caption: "wow, what a wonderful day!",
-    //   imgUrl: "https://instagram.fdel14-1.fna.fbcdn.net/v/t51.2885-15/e35/139032453_415799799736695_5678314497763823084_n.jpg?_nc_ht=instagram.fdel14-1.fna.fbcdn.net&_nc_cat=1&_nc_ohc=1qSD2-y6AJAAX8fKzje&tp=1&oh=c205a7bcadc6161be8d27feabbd1ec9d&oe=602A3843"
-    // },
-    // {
-    //   username: "yes.m.kidding",
-    //   caption: "Yeyy!",
-    //   imgUrl: "https://instagram.fdel14-1.fna.fbcdn.net/v/t51.2885-15/e35/s1080x1080/138373604_246805037065306_188224987130662923_n.jpg?_nc_ht=instagram.fdel14-1.fna.fbcdn.net&_nc_cat=110&_nc_ohc=TFrEgXFUIh0AX_UkidQ&tp=1&oh=1d502c1034f7f220e7e0270812f565fb&oe=6029E5ED"
-    // },
-    // {
-    //   username: "cristiano",
-    //   caption: "Dance Monkey",
-    //   imgUrl : "https://instagram.fdel14-1.fna.fbcdn.net/v/t51.2885-15/e35/s1080x1080/139007295_385971849365545_4232716593292261022_n.jpg?_nc_ht=instagram.fdel14-1.fna.fbcdn.net&_nc_cat=1&_nc_ohc=_gdoFdE94IUAX-QsDJC&tp=1&oh=9253bb04c0c60f0d3bd8f0b64ca295c5&oe=602A6E21"
-    // }
-  ]);
+	const classes = useStyle();
+	const [modalStyle] = useState(getModalStyle);
 
-  useEffect(() => {
-    db.collection('posts').onSnapshot(snapshot => {
-      setPosts(snapshot.docs.map(doc => ({
-        id: doc.id,
-        post: doc.data()
-      })));
-    });
-  }, []);
+	const [posts, setPosts] = useState([]);
+	const [open, setOpen] = useState(false);
+	const [openSignIn, setOpenSignIn] = useState(false);
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [email, setEmail] = useState('');
+	const [user, setUser] = useState(null);
 
-  return (
-    <div className="app">
-      <div className="app__header">
-        <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" />
-      </div>
-      {
-        posts.map(({id, post}) => (
-          <Post key={id} username={post.username} caption={post.caption} imgUrl={post.imgUrl} />
-        ))
-      }
-    </div>
-  );
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged((authUser) => {
+			if (authUser) {
+				console.log(authUser)
+				setUser(authUser);
+			} else {
+				setUser(null);
+			}
+		})
+
+		return () => {
+			unsubscribe();
+		}
+	}, [user, username]);
+
+	useEffect(() => {
+		db.collection('posts').onSnapshot(snapshot => {
+			setPosts(snapshot.docs.map(doc => ({
+				id: doc.id,
+				post: doc.data()
+			})));
+		});
+	}, []);
+
+	const signUp = (e) => {
+		e.preventDefault();
+		auth
+			.createUserWithEmailAndPassword(email, password)
+			.then(authUser => {
+				return authUser.user.updateProfile({
+					displayName: username
+				})
+			})
+			.catch(err => alert(err.message));		
+		setOpen(false);
+	}
+
+	const signIn = (e) => {
+		e.preventDefault();
+		auth
+			.signInWithEmailAndPassword(email, password)
+			.catch(err => alert(err.message))		
+		setOpenSignIn(false);
+	}
+
+	return (
+		<div className="app">
+			<Modal open={open} onClose={() => setOpen(false)}>
+				<div style={modalStyle} className={classes.paper}>
+					<center>
+						<img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" />
+					</center>
+					<form action="" className='app__signup'>
+						<Input placeholder='username' type='text' value={username} onChange={(e) => setUsername(e.target.value)} />
+						<Input placeholder='email' type='text' value={email} onChange={(e) => setEmail(e.target.value)} />
+						<Input placeholder='password' type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+						<Button onClick={signUp}>Sign Up</Button>
+					</form>
+				</div>
+			</Modal>
+			<Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+				<div style={modalStyle} className={classes.paper}>
+					<center>
+						<img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" />
+					</center>
+					<form action="" className='app__signup'>
+						<Input placeholder='email' type='text' value={email} onChange={(e) => setEmail(e.target.value)} />
+						<Input placeholder='password' type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+						<Button onClick={signIn}>Sign In</Button>
+					</form>
+				</div>
+			</Modal>
+			<div className="app__header">
+				<img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="" />
+			</div>
+			{
+				user ? (
+					<Button onClick={() => auth.signOut()}>Logout</Button>
+				) : (
+						<div className='loginContainer'>
+							<Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+							<Button onClick={() => setOpen(true)}>Sign Up</Button>
+						</div>
+					)
+			}
+			{
+				posts.map(({ id, post }) => (
+					<Post key={id} username={post.username} caption={post.caption} imgUrl={post.imgUrl} />
+				))
+			}
+		</div>
+	);
 }
 
 export default App;
